db98830a40be245dbed6affb7c802956e5b75f0f
Kim Llarves <YOUR_GITHUB_kimllarves125@gmail.com>
2025-12-08 19:23:14 +0400

    Fix navbar spacing and broken CSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,24 @@
-import "./globals.css";
-import type { Metadata } from "next";
-import Navbar from "./components/Navbar";
+  import "./globals.css";
+  import type { Metadata } from "next";
+  import Navbar from "./components/Navbar";
 
-export const metadata: Metadata = {
-  title: "BIYAYA",
-  description: "From The Islands To The World",
-};
+  export const metadata: Metadata = {
+    title: "BIYAYA",
+    description: "From The Islands To The World",
+  };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>
-        <Navbar />
+  export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <html lang="en">
+        <body>
+          <Navbar />
 
-        <main>{children}</main>
-      </body>
-    </html>
-  );
-}
+          <main>{children}</main>
+        </body>
+      </html>
+    );
+  }
